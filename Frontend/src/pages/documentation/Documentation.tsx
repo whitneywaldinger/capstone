a22e5b3712d790521c0fa06eba80e761be7311a1
Whitney Waldinger <118254799+whitneywaldinger@users.noreply.github.com>
2024-04-25 15:42:22 -0700

    Update and rename documentation.tsx to Documentation.tsx

--- Frontend/src/pages/documentation/Documentation.tsx
+++ Frontend/src/pages/documentation/Documentation.tsx
@@ -4,12 +4,8 @@ import "./documentation.css";
 export default function Documentation() {
   return (
     <div>
-      <div>
         <Header />
-      </div>
-      <div>
         <h1>Documentation</h1>
-      </div>
     </div>
   );
 };
