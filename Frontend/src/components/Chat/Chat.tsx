fc4269a560cbaea8a87dbd0acc7a65c036b9e322
wwaldi <wwaldi@uw.edu>
2024-05-09 10:46:21 -0700

    new url

--- Frontend/src/components/Chat/Chat.tsx
+++ Frontend/src/components/Chat/Chat.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import { Input } from './ChatComponents/input';
 import { Button } from './ChatComponents/button';
@@ -13,9 +11,16 @@ export default function Searchbar() {
     {
       text: 'Welcome to the ACEP Research Chatbot! I am your personalized research assistant. How can I help you?',
       sender: 'bot',
-      sources: [], // Initialize sources for each response
     },
   ]);
+  const messagesEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat window when responses change
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  }, [responses]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(event.target.value);
@@ -26,25 +31,24 @@ export default function Searchbar() {
 
     setUserInput(''); // Clear input after sending
     setResponses((prevResponses) => [
-      { text: userMessage, sender: 'user', sources: [] },
+      { text: userMessage, sender: 'user'},
       ...prevResponses,
     ]); // Pop user's message first
     setLoading(true); // Lock the send button until get the response
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/sendquery', { text: userInput });
+      const response = await axios.post('https://flaskapp-k22nw35fzq-uw.a.run.app/sendquery', { text: userInput });
       setResponses((prevResponses) => [
         {
           text: response.data.response,
           sender: 'bot',
-          sources: response.data.sources, // Include sources with the response
         },
         ...prevResponses,
       ]);
       setUserInput(''); // Clear input after sending
     } catch (error) {
       setResponses((prevResponses) => [
-        { text: 'Failed to get responses from LLM.', sender: 'bot', sources: [] },
+        { text: 'Failed to get responses from LLM.', sender: 'bot'},
         ...prevResponses,
       ]);
       console.error('Error sending message:', error);
@@ -61,28 +65,9 @@ export default function Searchbar() {
             {responses.map((response, index) => (
               <div key={index} className={`message ${response.sender === 'user' ? 'user' : 'bot'}`}>
                 <p className="message-text">{response.text}</p>
-                {response.sender === 'bot' && response.sources.length > 0 && (
-                  <div className="source-list">
-                    <p className="source-text">Sources:</p>
-                    {response.sources.map((source, index) => (
-                      <p key={index} className="source-link">
-                        <a
-                          href={
-                            source[0] === 'text_file_1.pdf'
-                              ? 'https://drive.google.com/file/d/19_zABcxMESJdeNwcqsvgP9sfR-YhSvod/view?usp=drive_link'
-                              : 'https://drive.google.com/file/d/1Vwo1UwaWrY3KViHvKbATBYDYAOk6imsy/view?usp=drive_link'
-                          }
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {source[0]} on page {source[1]}
-                        </a>
-                      </p>
-                    ))}
-                  </div>
-                )}
               </div>
             ))}
+            <div ref={messagesEndRef}></div>
           </div>
         </div>
 
